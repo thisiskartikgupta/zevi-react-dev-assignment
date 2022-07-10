78207a0642b9d06306813900170010be84708114
Kartik Gupta <thisiskartikgupta@gmail.com>
2022-07-10 13:58:46 +0530

    Added dependencies ESLint and Sass to our CRA project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
+/**
+ * The main <App> component of our application.
+ * @return {JsxElement}
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Zevi!</h1>
     </div>
   );
 }
